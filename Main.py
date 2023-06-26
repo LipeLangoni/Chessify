e5fb29b2778048486edf1b1d847a4f56067483cf
felipe.langoni <felipe.langoni@external.stellantis.com>
2023-06-26 03:12:58 -0300

    feat: implement frontend for Chessify aplication

--- Main.py
+++ Main.py
@@ -1,73 +1,48 @@
 import streamlit as st
-from model import GeneralModel
-import nltk
-from bs4 import BeautifulSoup
-from bs4.element import Comment
-import urllib.request
-nltk.download('stopwords')
-nltk.download('punkt')
-
-stopwords = nltk.corpus.stopwords.words('portuguese')
-
-def tag_visible(element):
-    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
-        return False
-    if isinstance(element, Comment):
-        return False
-    return True
-
-def text_from_html(body):
-    soup = BeautifulSoup(body, 'html.parser')
-    texts = soup.body.findAll(text=True)
-    visible_texts = filter(tag_visible, texts)  
-    return u" ".join(t.strip() for t in visible_texts)
+import requests
 
 
-
-
-
-def pre_processing(sentences):
-    for i in range(len(sentences)):
-        words = nltk.word_tokenize(sentences[i])
-        newwords = [word for word in words if word not in stopwords]
-        sentences[i] = ' '.join(newwords)
-    sentences = ''.join(sentences)
-    return sentences
+def get_chatbot_response(message):
+    url = 'http://localhost:5005/webhooks/rest/webhook'
+    payload = {
+        'message': message
+    }
+    response = requests.post(url, json=payload)
+    if response.status_code == 200:
+        data = response.json()
+        if data:
+            reply = data[0]['text']
+            if "images" in reply:
+                reply = reply.split("images")
+                image = reply[1]
+                answer = reply[0]
+                return answer,image
+            return [reply,None]
+    return None
 
 def app():
-
-   
-    pred = GeneralModel()
-
-
     @st.cache
     def process_prompt(input):
-
-        return pred.model_prediction(input=input.strip())
-
-    
+        return get_chatbot_response(input)
 
    
-    st.title("Resumo de paginas na web")
+    st.title("Tutor de xadrez")
 
  
 
-    s_example = "https://pt.wikipedia.org/wiki/Tel%C3%B4mero"
+    s_example = "Quantas peças cada jogador começa no xadrez?"
     input = st.text_input(
-        "Cole a URL",
+        "Faça uma pergunta",
         value=s_example,
         max_chars=150,
     )
 
-    html = urllib.request.urlopen(input).read()
-    text = text_from_html(html)
-    sentences = nltk.sent_tokenize(text)
-    promp = pre_processing(sentences)
-    mid = len(promp) // 2
-    promp = promp[:mid]
-
     if st.button("Enviar"):
         with st.spinner(text="Rodando"):
-            report_text = process_prompt(promp)
-            st.markdown(report_text)
+            report_text = process_prompt(input)
+            answer = report_text[0]
+            image = report_text[1]
+            st.markdown(answer)
+            if image != None:
+                st.image("images"+image, caption='Image Caption')
     
---
+++ app.py
@@ -0,0 +1,6 @@
+import Main
+import streamlit as st
+
+st.set_page_config(page_title="Tutor de xadrez", page_icon=":shark:", layout="wide")
+
+Main.app()
